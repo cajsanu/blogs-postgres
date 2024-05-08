fb834257d4b5d5239b9ea39b14471f038efd5e36
Cajsa Nummelin <cajsanummelin@Aleksandrs-Air.home>
2024-05-08 09:37:36 +0300

    Tweaks

--- models/index.js
+++ models/index.js
@@ -2,14 +2,10 @@ const Blog = require("./Blog");
 const User = require("./User");
 // const Author = require("./Author")
 const UserReadingList = require("./ReadingList");
-// const Reads = require("./Reads")
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 // Author.hasMany(Blog)
-
-// Reads.belongsToMany(Blog, {through: "Read_blog"})
-// Blog.belongsToMany(Reads, {through: "Read_blog"})
 
 User.belongsToMany(Blog, { through: UserReadingList, as: "marked_blogs" });
 Blog.belongsToMany(User, { through: UserReadingList, as: "users_marked" });
@@ -19,5 +15,4 @@ module.exports = {
   User,
   // Author
   UserReadingList,
-  // Reads
 };
